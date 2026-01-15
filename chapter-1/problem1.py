da8882bc87cd8d5e173c6d8c07ff9333c176041a
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2026-01-15 13:20:06 +0530

    py

---
+++ chapter-1/problem1.py
@@ -0,0 +1,46 @@
+# print('''Twinkle Twinkle, Little Star
+# How I wonder what you are
+# Up above the world so high
+# Like a diamond in the sky
+# Twinkle Twinkle Little Star
+# How I wonder what you are!
+# Twinkle Twinkle, Little Star
+# How I wonder what you are
+# Up above the world so high
+# Like a diamond in the sky
+# Twinkle Twinkle Little Star
+# How I wonder what you are!''')
+
+
+
+# problem 2
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("are you stupid")
+  
+# engine.runAndWait()
+
+
+
+# problem 3
+# import os
+
+# path = "/Trade-flask-fixed (1)"   # current directory
+# contents = os.listdir(path)
+
+# print(contents)
+
+
+# problem 4
+import os  # importing os module to interact with operating system
+
+path = "/"  # dot (.) means current directory
+
+# listdir() returns all files and folders in the directory
+contents = os.listdir(path)
+
+# printing directory contents
+print(contents)
+
+
