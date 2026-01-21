c215e00844c09f42da2c8535f30bfb5db194d367
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2026-01-21 16:34:25 +0530

    problem solve

--- chapter-2/python.py
+++ chapter-2/python.py
@@ -8,8 +8,8 @@ d = 5
 print("remainder is:", c % d) 
 
 
-# a = int(input("Enter first number: "))
-# print(type(a))
+a = int(input("Enter first number: "))
+print(type(a))
 
 a =  input("Enter first number: ")
 b =input("Enter sec number: ")
@@ -18,10 +18,10 @@ print("is a greater than b:", a > b)
 
 b = input("Enter first number: ")
 d = input("Enter sec number: ")
-print("average is:", (int(b) + int(d)) / 2)
+print("average is:",( b + d) / 2)
 
 
 e = input("Enter first number: ")
 f = input("Enter sec number: ")
-print("the square of the sum is:", (int(e) + int(f)) ** 2)
+# print("the square of the sum is:", (int(e) + int(f)) ** 2)
 
